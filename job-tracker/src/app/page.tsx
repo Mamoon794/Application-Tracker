18bb606ac20523dd6d1f0a6128cbb2fb5a0dc3c8
Mamoon Akhtar <mamoon.akhtar97@gmail.com>
2025-12-27 13:25:27 -0500

    Adding axios example

--- job-tracker/src/app/page.tsx
+++ job-tracker/src/app/page.tsx
@@ -1,17 +1,26 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
-  const data = getGoogle();  // Just an example on using axios
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    getExample().then((data) => setMessage(data));
+  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Job Tracking Application</h1>
+      <div>
+        <h1 className="text-4xl">Job Tracking Application</h1>
+        <p>{message}</p>
+      </div>
     </div>
   );
 }
 
-function getGoogle(){
-  axios.get("https://www.google.com").then((res) => {
-    return res.data;
-  });
+async function getExample(){
+  const data = await axios.get("http://localhost:3000/api/example");
+  return data.data;
 }
