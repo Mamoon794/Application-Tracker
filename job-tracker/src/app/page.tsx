c7305c35369f2c834b2a70448b6f4affd5a94828
Mamoon Akhtar <mamoon.akhtar97@gmail.com>
2025-12-27 13:07:29 -0500

    Adding axios

--- job-tracker/src/app/page.tsx
+++ job-tracker/src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import axios from "axios";
 
 export default function Home() {
+  const data = getGoogle();  // Just an example on using axios
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Job Tracking Application</h1>
     </div>
   );
+}
+
+function getGoogle(){
+  axios.get("https://www.google.com").then((res) => {
+    return res.data;
+  });
 }
